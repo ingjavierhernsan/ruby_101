415d2ba3e1ddd2eb3f8b5d776a9ecf3356853f97
jhs <ingjavierhernsan@hotmail.com>
2025-03-14 03:20:36 -0700

    Exercises 48 and 49

---
+++ Interpolacion_de_String/ejercicio048.rb
@@ -0,0 +1,10 @@
+print "Ingrese nombre: "
+nombre = gets.chomp
+
+print "Ingrese la edad: "
+edad = gets.to_i
+
+print "Ingrese la altura Ej 1.75: "
+altura = gets.to_f
+
+puts "#{nombre} tiene #{edad} anios y una altura de #{altura}"
---
+++ Interpolacion_de_String/ejercicio049.rb
@@ -0,0 +1,6 @@
+paso = 1
+
+while paso <= 10
+  puts "#{paso} * 2 = #{paso * 2}"
+  paso = paso + 1
+end
