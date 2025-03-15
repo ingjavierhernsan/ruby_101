2591acd4a518de577cb8484e80a095e3373c0528
jhs <ingjavierhernsan@hotmail.com>
2025-03-15 15:09:54 -0700

    Exercise 85

---
+++ Estructura_de_datos_tipo_Hash/ejercicio085.rb
@@ -0,0 +1,7 @@
+paises = { "argentina" => 40000000,
+"espana" => 46000000,
+"brasil" => 190000000,
+"uruguay" => 34000000}
+
+puts paises
+puts "La cantidad de habitantes de espana es: #{paises["espana"]}"
