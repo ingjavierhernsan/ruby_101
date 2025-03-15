9ed6d8343b49d0d1f81aba53a96915e71cf3bc00
jhs <ingjavierhernsan@hotmail.com>
2025-03-15 00:11:08 -0700

    Exercise 67

---
+++ Arreglos_recorrido_mediante_un_for/ejercicio067.rb
@@ -0,0 +1,9 @@
+arreglo = [10, 7, 3, 7, 2]
+suma = 0
+
+for elemento in arreglo
+  suma = suma + elemento
+end
+
+print "Los elementos del arreglo son: ", arreglo, "\n"
+print "La suma de todos sus elementos es: ", suma
