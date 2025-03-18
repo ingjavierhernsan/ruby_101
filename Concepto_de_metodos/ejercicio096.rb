32b9d03289fb01e711986bbaa427cdb10cfa0549
Javier Hernandex <ingjavierhernsan@hotmail.com>
2025-03-18 15:39:50 -0700

    Exercises 96 and 97

---
+++ Concepto_de_metodos/ejercicio096.rb
@@ -0,0 +1,29 @@
+def presentacion
+  puts "Programa que permite cargar dos valores por teclado."
+  puts "Efectua la suma de los valores"
+  puts "Muestra el resultado de la suma"
+  puts "*" * 80
+end
+
+def carga_suma
+  print "Ingrese el primer valor: "
+  valor1 = gets.to_i
+
+  print "Ingrese el segundo valor: "
+  valor2 = gets.to_i
+
+  suma = valor1 + valor2
+
+  puts "La suma de los dos valores es: #{ suma }"
+end
+
+def finalizacion
+  puts "*" * 80
+  puts "Gracias por utilizar este programa."
+end
+
+# Bloque principal
+
+presentacion
+carga_suma
+finalizacion
---
+++ Concepto_de_metodos/ejercicio097.rb
@@ -0,0 +1,22 @@
+def carga_suma
+  print "Ingrese el primer valor: "
+  valor1 = gets.to_i
+
+  print "Ingrese el segundo valor: "
+  valor2 = gets.to_i
+
+  suma = valor1 + valor2
+
+  puts "La suma de los dos valores es: #{ suma }"
+end
+
+def separacion
+  puts "*" * 80
+end
+
+# Bloque principal
+
+for x in 1..5
+  carga_suma
+  separacion
+end
